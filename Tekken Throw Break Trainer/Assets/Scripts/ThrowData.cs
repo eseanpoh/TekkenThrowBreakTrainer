e97a6725997d30ab5fe9f458116e9438fd073c38
Austin <jacoba4@rpi.edu>
2020-02-05 20:42:59 -0500

    ThrowData Scriptable Object

---
+++ Tekken Throw Break Trainer/Assets/Scripts/ThrowData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "ThrowData", menuName = "Create Throw Data")]
+public class ThrowData : ScriptableObject
+{
+    [Header("Data")]
+    public string move_name;
+    public int break_window;
+    public int start_frame;
+    public int end_frame;
+
+    //[Tooltip("I have no clue if we're actually going to use this")]
+    //public Sprite bucketSprite;
+    //[Header("Also put in things like meshes here so that it's easy to find them!")]
+
+}
